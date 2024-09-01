4d5c7eab10a8cac55e555fd7f464a698c96bee49
riiamri <riiamri23@gmail.com>
2024-09-01 12:54:23 +0700

    adding function for write to CSV and JSON

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	// import Colly
+
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -59,43 +61,78 @@ func main() {
 
 	// triggered once scraping is done (e.g., write the data to a CSV file)
 	c.OnScraped(func(r *colly.Response) {
-
-		// open the CSV file
-		file, err := os.Create("products.csv")
-		if err != nil {
-			log.Fatalln("Failed to create output CSV file", err)
+		// Convert the slice to JSON and write to a file
+		if err := writeToJSONFile(products, "product.json"); err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("Data has been written to people.json")
 		}
-		defer file.Close()
 
-		// initialize a file writer
-		writer := csv.NewWriter(file)
-
-		// write the CSV headers
-		headers := []string{
-			"Url",
-			"Image",
-			"Name",
-			"Price",
+		if err := writeToCSVFile(products, "product.csv"); err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("Data Has been written to product.json")
 		}
-		writer.Write(headers)
 
-		// write each product as a CSV row
-		for _, product := range products {
-			// convert a Product to an array of strings
-			record := []string{
-				product.Url,
-				product.Image,
-				product.Name,
-				product.Price,
-			}
-
-			// add a CSV record to the output file
-			writer.Write(record)
-		}
-		defer writer.Flush()
 	})
 
 	// open the target URL
 	c.Visit("https://www.scrapingcourse.com/ecommerce")
 
+}
+
+func writeToJSONFile(data interface{}, filename string) error {
+	// Create the JSON file
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Encode data to JSON and write to the file
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ") // Optional: pretty-print JSON with indentation
+	if err := encoder.Encode(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeToCSVFile(data interface{}, filename string) error {
+	// open the CSV file
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
+
+	// initialize a file writer
+	writer := csv.NewWriter(file)
+
+	// write the CSV headers
+	headers := []string{
+		"Url",
+		"Image",
+		"Name",
+		"Price",
+	}
+	writer.Write(headers)
+
+	// write each product as a CSV row
+	for _, product := range data.([]Product) {
+		// convert a Product to an array of strings
+		record := []string{
+			product.Url,
+			product.Image,
+			product.Name,
+			product.Price,
+		}
+
+		// add a CSV record to the output file
+		writer.Write(record)
+	}
+	defer writer.Flush()
+
+	return nil
 }
